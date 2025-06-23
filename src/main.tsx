a020c24fb5877ca1e83a6094ea88d4e546bf8200
777anishsingh <777anish.singh@gmail.com>
2025-06-23 20:56:32 +0530

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import "./index.css";
+
+import { BrowserRouter as Router } from 'react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './context/AuthContext.tsx';
+
+const client = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+
+  <QueryClientProvider client={client}>
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
+  </QueryClientProvider>
+
+)
+
